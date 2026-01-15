71913a1eca47197c5052fe5718c38ea364b1d58f
Igor Storchevoy <hellsgor@gmail.com>
2026-01-15 23:51:35 +0400

    update logo

--- src/app/components/UI/header.tsx
+++ src/app/components/UI/header.tsx
@@ -8,26 +8,26 @@ import {
   Link,
   Button,
 } from '@heroui/react';
+import Image from 'next/image';
 
-export const AcmeLogo = () => {
+export const Logo = () => {
   return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
+    <Image
+      src="/logo_tatar_kitchen.webp"
+      alt="logo image"
+      height={26}
+      width={26}
+      priority
+    />
   );
 };
 
-export default function App() {
+export default function Header() {
   return (
     <Navbar>
       <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+          <Logo />
+          <p className="font-bold text-inherit">Татарская кухня</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
