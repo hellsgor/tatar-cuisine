a509a0339aa4769b3d8cf7299d9aa4c18ee2f641
Igor Storchevoy <hellsgor@gmail.com>
2026-01-16 00:07:50 +0400

    add config-file with consts

--- src/app/components/UI/header.tsx
+++ src/app/components/UI/header.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { siteConfig } from '@/app/config/site.config';
 import {
   Navbar,
   NavbarBrand,
@@ -15,7 +16,7 @@ export const Logo = () => {
   return (
     <Image
       src="/logo_tatar_kitchen.webp"
-      alt="logo image"
+      alt={`${siteConfig.title} логотип`}
       height={26}
       width={26}
       priority
@@ -25,38 +26,39 @@ export const Logo = () => {
 
 export default function Header() {
   const pathname = usePathname();
-  const navItems = [
-    { href: '/', label: 'Рецепты' },
-    { href: '/ingredients', label: 'Ингредиенты' },
-    { href: '/about', label: 'О нас' },
-  ];
+
+  const renderNavItems = () =>
+    siteConfig.navItems.map(({ href, label }, i) => (
+      <NavbarItem key={i}>
+        <Link
+          href={href}
+          className={`px-3 py-1 rounded-md border-1 border-transparent  ${
+            pathname === href ? 'text-blue-500' : 'text-foreground'
+          } hover:text-blue-300 hover:border-blue-300 transition-colors transition-border duration-200`}
+        >
+          {label}
+        </Link>
+      </NavbarItem>
+    ));
 
   return (
     <Navbar>
       <NavbarBrand>
         <Link href="/" className="flex gap-1 items-center">
           <Logo />
-          <p className="font-bold text-inherit">Татарская кухня</p>
+          <p className="font-bold text-inherit">{siteConfig.title}</p>
         </Link>
       </NavbarBrand>
+
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {navItems.map(({ href, label }, i) => (
-          <NavbarItem key={i}>
-            <Link
-              href={href}
-              className={`px-3 py-1 rounded-md border-1 border-transparent  ${
-                pathname === href ? 'text-blue-500' : 'text-foreground'
-              } hover:text-blue-300 hover:border-blue-300 transition-colors transition-border duration-200`}
-            >
-              {label}
-            </Link>
-          </NavbarItem>
-        ))}
+        {renderNavItems()}
       </NavbarContent>
+
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
           <Link href="#">Логин</Link>
         </NavbarItem>
+
         <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
             Регистрация
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { siteConfig } from './config/site.config';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers/providers';
@@ -15,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Татарская кухня',
-  description: 'Рецепты татарской кухни',
+  title: siteConfig.title,
+  description: siteConfig.description,
 };
 
 export default function RootLayout({
