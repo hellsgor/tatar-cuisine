acdf4a87699ebfb47d12f5b97801d9859aa9b4fe
Igor Storchevoy <hellsgor@gmail.com>
2026-01-15 23:52:04 +0400

    update header styling

--- src/app/components/UI/header.tsx
+++ src/app/components/UI/header.tsx
@@ -5,10 +5,11 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
   Button,
 } from '@heroui/react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const Logo = () => {
   return (
@@ -23,36 +24,42 @@ export const Logo = () => {
 };
 
 export default function Header() {
+  const pathname = usePathname();
+  const navItems = [
+    { href: '/', label: 'Рецепты' },
+    { href: '/ingredients', label: 'Ингредиенты' },
+    { href: '/about', label: 'О нас' },
+  ];
+
   return (
     <Navbar>
       <NavbarBrand>
+        <Link href="/" className="flex gap-1 items-center">
           <Logo />
           <p className="font-bold text-inherit">Татарская кухня</p>
+        </Link>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
+        {navItems.map(({ href, label }, i) => (
+          <NavbarItem key={i}>
+            <Link
+              href={href}
+              className={`px-3 py-1 rounded-md border-1 border-transparent  ${
+                pathname === href ? 'text-blue-500' : 'text-foreground'
+              } hover:text-blue-300 hover:border-blue-300 transition-colors transition-border duration-200`}
+            >
+              {label}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="#">Логин</Link>
         </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
+            Регистрация
           </Button>
         </NavbarItem>
       </NavbarContent>
