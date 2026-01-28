d8de4c7558f807c0d88eee48dec86a4b3ee81b2a
Igor Storchevoy <hellsgor@gmail.com>
2026-01-29 00:48:29 +0400

    refactor: remove middleware and implement session-based route protection in IngredientsPage

--- src/app/(protected)/ingredients/page.tsx
+++ src/app/(protected)/ingredients/page.tsx
@@ -1,7 +1,14 @@
+import { auth } from '@/auth/auth';
+import { redirect } from 'next/navigation';
 import IngredientsTable from '@/components/features/ingredients/ingredients-table';
 import IngredientsForm from '@/components/features/ingredients/ingredients.form';
 
-export default function IngredientsPage() {
+export default async function IngredientsPage() {
+  const session = await auth();
+  if (!session) {
+    redirect('/error?message=Недостаточно прав');
+  }
+
   return (
     <div className="flex flex-col items-center gap-y-6">
       <IngredientsForm />
