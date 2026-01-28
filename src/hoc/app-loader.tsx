74a08aabefbf57f02e8f3330a7cc13f7fd8aaa07
Igor Storchevoy <hellsgor@gmail.com>
2026-01-28 10:13:12 +0400

    add ingredients loading

--- src/hoc/app-loader.tsx
+++ src/hoc/app-loader.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useAuthStore } from '@/store/auth.store';
+import { useIngredientStore } from '@/store/ingredient.store';
 import { useSession } from 'next-auth/react';
 import { ReactNode, useEffect } from 'react';
 
@@ -10,10 +11,18 @@ interface AppLoaderProps {
 
 export default function AppLoader({ children }: AppLoaderProps) {
   const { data: session, status } = useSession();
-  const { setAuthState } = useAuthStore();
+  const { isAuth, setAuthState } = useAuthStore();
+  const { loadIngredients } = useIngredientStore();
 
   useEffect(() => {
     setAuthState(status, session);
   }, [status, session, setAuthState]);
+
+  useEffect(() => {
+    if (isAuth) {
+      loadIngredients();
+    }
+  }, [isAuth, loadIngredients]);
+
   return <div>{children}</div>;
 }
