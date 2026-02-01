4bea90ed99daa495823f54e04325c2dd4a43c8af
Igor Storchevoy <hellsgor@gmail.com>
2026-02-01 22:55:57 +0400

    add recipes loading

--- src/hoc/app-loader.tsx
+++ src/hoc/app-loader.tsx
@@ -2,6 +2,7 @@
 
 import { useAuthStore } from '@/store/auth.store';
 import { useIngredientStore } from '@/store/ingredient.store';
+import { useRecipeStore } from '@/store/recipe.store';
 import { useSession } from 'next-auth/react';
 import { ReactNode, useEffect } from 'react';
 
@@ -13,6 +14,7 @@ export default function AppLoader({ children }: AppLoaderProps) {
   const { data: session, status } = useSession();
   const { isAuth, setAuthState } = useAuthStore();
   const { loadIngredients } = useIngredientStore();
+  const { loadRecipes } = useRecipeStore();
 
   useEffect(() => {
     setAuthState(status, session);
@@ -23,6 +25,10 @@ export default function AppLoader({ children }: AppLoaderProps) {
       loadIngredients();
     }
   }, [isAuth, loadIngredients]);
+
+  useEffect(() => {
+    loadRecipes();
+  }, [loadRecipes]);
 
   return <div>{children}</div>;
 }
