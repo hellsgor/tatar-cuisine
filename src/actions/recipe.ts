28d5737a401242e0caff5bba1cb5a8635ee76e80
Igor Storchevoy <hellsgor@gmail.com>
2026-02-01 22:16:08 +0400

    add recipe actions

---
+++ src/actions/recipe.ts
@@ -0,0 +1,148 @@
+import { prisma } from '@/helpers/prisma';
+
+export async function getRecipes() {
+  try {
+    const recipes = await prisma.recipe.findMany({
+      include: {
+        recipeIngredients: {
+          include: {
+            ingredient: true,
+          },
+        },
+      },
+    });
+    return ({ success: true, recipes });
+  } catch (error) {
+    const errorText = 'Ошибка при загрузке рецептов';
+    console.log(errorText);
+    return { success: false, error: errorText };
+  }
+}
+
+export async function createRecipe(formData: FormData) {
+  try {
+    const { name,
+      description,
+      imageUrl,
+      ingredients,
+      error,
+      success } = getRecipeData({ formData, error: 'Имя и хотя бы один ингредиент обязательны' });
+
+    if (error && success === false) {
+      return { error, success };
+    }
+
+    const recipe = (name && description && imageUrl && ingredients) && await prisma.recipe.create({
+      data: {
+        name,
+        description,
+        imageUrl,
+        recipeIngredients: {
+          create: ingredients.map(({ ingredientId, quantity }) => ({
+            ingredient: { connect: { id: ingredientId } },
+            quantity,
+          })),
+        },
+      },
+      include: {
+        recipeIngredients: {
+          include: {
+            ingredient: true,
+          },
+        }
+      },
+    });
+
+    return ({ success: true, recipe });
+  } catch (error) {
+    const errorText = 'Ошибка при создании рецепта';
+    console.log(errorText);
+    return { success: false, error: errorText };
+  }
+}
+
+export async function updateRecipe(id: string, formData: FormData) {
+  try {
+    const { name,
+      description,
+      imageUrl,
+      ingredients,
+      error,
+      success } = getRecipeData({ formData, error: 'Имя и хотя бы один ингредиент обязательны' });
+
+    if (error && success === false) {
+      return { error, success };
+    }
+
+    const recipe = await prisma.recipe.update({
+      where: { id },
+      data: {
+        name,
+        description,
+        imageUrl,
+        recipeIngredients: {
+          deleteMany: {},
+          create: ingredients?.map(({ ingredientId, quantity }) => ({
+            ingredient: { connect: { id: ingredientId, } },
+            quantity,
+          }))
+        },
+      },
+      include: {
+        recipeIngredients: {
+          include: {
+            ingredient: true,
+          },
+        }
+      },
+    });
+
+    return { success: true, recipe };
+
+  } catch (error) {
+    const errorText = 'Ошибка при обновлении рецепта';
+    console.log(errorText, error);
+    return ({ success: false, error: errorText });
+  }
+}
+
+export async function deleteRecipe(id: string) {
+  try {
+    await prisma.recipeIngredient.deleteMany({
+      where: { recipeId: id },
+    });
+
+    await prisma.recipe.delete({
+      where: { id },
+    });
+
+    return ({ success: true });
+  } catch (error) {
+    const errorText = 'Ошибка при удалении рецепта';
+    console.log(errorText, error);
+    return ({ success: false, error: errorText });
+  }
+}
+
+const getRecipeData = ({ formData, error }: { formData: FormData, error: string; }) => {
+  const name = formData.get('name') as string;
+  const description = formData.get('description') as string;
+  const imageUrl = formData.get('imageUrl') as string | null;
+  const ingredients = Array.from(formData.entries())
+    .filter(([key]) => key.startsWith('ingredient_'))
+    .map(([key, value]) => ({
+      ingredientId: value as string,
+      quantity: parseFloat(
+        formData.get(`quantity_${key.split('_')[1]}`) as string,
+      ),
+    }));
+
+  if (!name || !ingredients.length) {
+    return {
+      success: false,
+      error,
+    };
+  }
+
+  return ({ name, description, imageUrl, ingredients });
+};
